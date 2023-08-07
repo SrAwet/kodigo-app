d48b0f05d4d2662a034ae68441aa794587c37f8e
Willian Alexander <willian.zape65@gmail.com>
2023-08-07 09:39:54 -0600

    :sparkles: feat(templates):add templates

--- views/bootcampView.php
+++ views/bootcampView.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kodigo App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
-<body>
+<?php require('templates\header.php');?>
     <h1 class="text-center">Welcome to kodigo app</h1>
     <h2 class="text-center">All bootcamps</h2>
 
@@ -28,9 +20,4 @@
 
 
     </div>
-    
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php require('templates\footer.php');?>
